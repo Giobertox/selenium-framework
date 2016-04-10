43f0effd4292b97530cba2227a5f3b28a540d421
giovanni <giovanni@giovanni-HP-ENVY-17-Notebook-PC>
2016-04-10 23:40:27 +0100

    Driver connecting to Slenium Server

--- src/test/java/com/giobertox/test/automation/sample/utils/Driver.java
+++ src/test/java/com/giobertox/test/automation/sample/utils/Driver.java
@@ -1,11 +1,15 @@
 package com.giobertox.test.automation.sample.utils;
 
+import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 public final class Driver {
+
+	private static final String SELENIUM_SERVER_URL = "http://127.0.0.1:5555/wd/hub";
 
 	private Driver() {
 	}
@@ -14,7 +18,9 @@ public final class Driver {
 
 	static {
 		try {
-			instance = new FirefoxDriver();
+			// se lanciato in locale
+			// instance = new FirefoxDriver();
+			instance = new RemoteWebDriver(new URL(SELENIUM_SERVER_URL), DesiredCapabilities.firefox());
 			instance.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 		} catch (Exception e) {
 			throw new RuntimeException("Oh, an error occurred when instantiating the WebDriver!", e);
